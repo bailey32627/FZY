af528a05cddd1089bddfcd9af36afae2937356ac
Brad Bailey <bailey32627@gmail.com>
2023-04-11 06:12:53 -0400

    moved math to core

--- src/FZY.js
+++ src/FZY.js
@@ -1,7 +1,7 @@
 export { GLUtil } from './core/gl/GlUtil.js';
 export { Engine } from './core/Engine.js';
-export { Vector2 } from './math/Vector2.js';
-export { Vector3 } from './math/Vector3.js';
-export { Quaternion } from './math/Quaternion.js';
-export { Matrix3 } from './math/Matrix3.js';
-export { Matrix4 } from './math/Matrix4.js';
+export { Vector2 } from './core/math/Vector2.js';
+export { Vector3 } from './core/math/Vector3.js';
+export { Quaternion } from './core/math/Quaternion.js';
+export { Matrix3 } from './core/math/Matrix3.js';
+export { Matrix4 } from './core/math/Matrix4.js';
