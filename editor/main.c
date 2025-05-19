e134a6888ebfdf128047c1c9bb948a298867e4ab
Brad Bailey <bailey32627@gmail.com>
2025-05-19 13:57:21 -0400

    Project setup

---
+++ editor/main.c
@@ -0,0 +1,8 @@
+#include "engine.h"
+
+int main() {
+  char str[64] = "Hello from Editor\n";
+  print_hello( str );
+
+  return 0;
+}
---
+++ game/main.c
@@ -0,0 +1,6 @@
+#include "engine.h"
+
+int main() {
+  print_hello( "hello from game!\n" );
+  return 0;
+}
